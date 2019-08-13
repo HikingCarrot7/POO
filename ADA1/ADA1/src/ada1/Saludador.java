14ee1b2e8bc8b5fa53fb2cc9422c1784565e14eb
HikingCarrot7 <ricardoibarra2044@gmail.com>
2019-08-13 17:17:36 -0500

    ADA1

---
+++ ADA1/ADA1/src/ada1/Saludador.java
@@ -0,0 +1,13 @@
+package ada1;
+
+/**
+ *
+ * @author HikingCarrot7
+ */
+public class Saludador
+{
+    public void saludar(String mensaje)
+    {
+        System.out.println(mensaje);
+    }
+}
---
+++ ADA1/ADA1/src/ada1/UsoSaludador.java
@@ -0,0 +1,17 @@
+package ada1;
+
+/**
+ *
+ * @author HikingCarrot7
+ */
+public class UsoSaludador
+{
+
+    public static void main(String[] args)
+    {
+        Saludador saludador1 = new Saludador();
+
+        saludador1.saludar("Hola mundo");
+    }
+
+}
