b4cf08365dffc36fc0b76405e77ea64b0b261950
HikingCarrot7 <ricardoibarra2044@gmail.com>
2019-09-01 14:20:54 -0500

    .

--- ADAS/src/ada1_2/Fecha.java
+++ ADAS/src/ada1_2/Fecha.java
@@ -1,6 +1,7 @@
 package ada1_2;
 
 /**
+ * Se crean fechas con los parámetros especificados en el constructor.
  *
  * @author HikingCarrot7
  */
@@ -13,6 +14,16 @@ public class Fecha
         "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
     };
 
+    /**
+     * Crea un objeto fecha con el día, mes y año que especifiquemos.
+     *
+     * @param dia El día de la fecha.
+     * @param mes El mes de la fecha.
+     * @param anio El año de la fecha.
+     *
+     * @since 1.0
+     *
+     */
     public Fecha(int dia, int mes, int anio)
     {
         this.dia = dia;
@@ -21,11 +32,24 @@ public class Fecha
 
     }
 
+    /**
+     * @deprecated Crea una fecha que representa el 1° de enero de 1900
+     *
+     * @since 1.0
+     */
     public Fecha()
     {
         this(1, 1, 1900);
     }
 
+    /**
+     * Regresa la representación String de esta fecha.
+     *
+     * @return La representación String de esta fecha.
+     *
+     * @since 1.0
+     *
+     */
     public String mostrarFecha()
     {
         return String.format("%d de %s de %d", dia, meses[mes - 1].toLowerCase(), anio);
--- ADAS/src/ada1_2/PruebaFecha.java
+++ ADAS/src/ada1_2/PruebaFecha.java
@@ -7,9 +7,17 @@ package ada1_2;
 public class PruebaFecha
 {
 
+    /**
+     *
+     * @param args Los argumentos recuperados de la consola.
+     *
+     * @since 1.0
+     */
     public static void main(String[] args)
     {
+        //Imprimimos en pantalla la representación String para esta fecha.
         System.out.println(new Fecha(6, 1, 2000).mostrarFecha());
-    }
 
-}
+    }//Fin de main.
+
+}//Fin de la clase PruebaFecha.
