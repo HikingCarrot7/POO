deaef6b45c0c17eb743db331a641bed0f3ba925c
HikingCarrot7 <ricardoibarra2044@gmail.com>
2019-09-02 16:00:49 -0500

    Fechas

---
+++ ADAS/src/ada1_3/PruebaFecha.java
@@ -0,0 +1,14 @@
+package ada1_3;
+
+/**
+ *
+ * @author HikingCarrot7
+ */
+public class PruebaFecha
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println(new Fraccion(5, 4).suma(new Fraccion(1, 2)).simplifica());
+    }
+}
