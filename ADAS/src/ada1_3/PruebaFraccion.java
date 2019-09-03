5d1739e0502a565a1fcdc6d13991fec2db2be374
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-09-03 10:07:51 -0500

    .

--- ADAS/src/ada1_3/PruebaFraccion.java
+++ ADAS/src/ada1_3/PruebaFraccion.java
@@ -12,12 +12,16 @@ public class PruebaFraccion
         Fraccion fraccion1 = new Fraccion(1, 2);
         Fraccion fraccion2 = new Fraccion(1, 3);
 
+        System.out.println("Suma: ");
         System.out.println(fraccion1.suma(fraccion2).simplifica());
 
+        System.out.println("\nResta: ");
         System.out.println(fraccion1.resta(fraccion2).simplifica());
 
+        System.out.println("\nMultiplicación: ");
         System.out.println(fraccion1.multiplica(fraccion2).simplifica());
 
+        System.out.println("\nDivisión: ");
         System.out.println(fraccion1.divide(fraccion2).simplifica());
 
     }
