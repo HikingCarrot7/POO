274269f4a7e02ece10083ae565ab9fc66bfdcc66
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-09-03 11:53:11 -0500

    .

---
+++ ADAS/src/ejemploregistrosempleados/Empleado.java
@@ -0,0 +1,20 @@
+package ejemploregistrosempleados;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author A15001169
+ */
+public final class Empleado
+{
+
+    private String clave, nombre, apellidoPaterno, apellidoMaterno;
+    private Calendar fechaNacimiento, fechaIngreso;
+
+    public Empleado()
+    {
+
+    }
+
+}
