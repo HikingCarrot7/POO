1f446b3f4eb63788435ebf8e370bb776b921c445
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-09-12 10:10:31 -0500

    TragaPerras

---
+++ ADAS/src/ada1_6/TragaMonedas.java
@@ -0,0 +1,32 @@
+package ada1_6;
+
+/**
+ *
+ * @author HikingCarrot7
+ */
+public class TragaMonedas
+{
+
+    private double dineroMaquina;
+
+    public TragaMonedas(double dineroMaquina)
+    {
+        this.dineroMaquina = dineroMaquina;
+    }
+
+    private void ejecutarJuego()
+    {
+
+    }
+
+    private void calcularGanancia()
+    {
+
+    }
+
+    private boolean sinDinero()
+    {
+        return dineroMaquina == 0;
+    }
+
+}
