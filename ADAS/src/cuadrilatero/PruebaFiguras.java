00b10d2b63374e5da32aa9b207d2f515f69ec66d
HikingCarrot7 <ricardoibarra2044@gmail.com>
2019-09-18 21:48:38 -0500

    .

--- ADAS/src/cuadrilatero/PruebaFiguras.java
+++ ADAS/src/cuadrilatero/PruebaFiguras.java
@@ -22,11 +22,14 @@ public class PruebaFiguras
      */
     public void ProbarFiguras()
     {
-        System.out.println(new Cuadrado(new Punto(-1, 0), new Punto(0, 0), new Punto(-1, -1), new Punto(0, -1)).obtenerArea());
 
-        System.out.println(new Trapecio(new Punto(1, 2), new Punto(3, 2), new Punto(0, 0), new Punto(4, 0)).obtenerArea());
+        System.out.println(String.format("%-35s %,.2f u^2", "El área de este cuadrado es:", new Cuadrado(new Punto(-1, 0), new Punto(0, 0), new Punto(-1, -1), new Punto(0, -1)).obtenerArea()));
 
-        System.out.println(new Romboide(new Punto(0, 0), new Punto(2, 3), new Punto(2, -2), new Punto(4, 0)).obtenerArea());
+        System.out.println(String.format("%-35s %,.2f u^2", "El área de este rectángulo es:", new Rectangulo(new Punto(0, 2), new Punto(3, 2), new Punto(0, 0), new Punto(3, 0)).obtenerArea()));
+
+        System.out.println(String.format("%-35s %,.2f u^2", "El área de este trapecio es:", new Trapecio(new Punto(1, 2), new Punto(3, 2), new Punto(0, 0), new Punto(4, 0)).obtenerArea()));
+
+        System.out.println(String.format("%-35s %,.2f u^2", "El área de este rombiode es:", new Romboide(new Punto(0, 0), new Punto(2, 3), new Punto(2, -2), new Punto(4, 0)).obtenerArea()));
 
     }
 
