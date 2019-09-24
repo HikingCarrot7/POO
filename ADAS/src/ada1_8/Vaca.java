9141d31e733d9fba734dbc3a74e01a2182eb1480
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-09-24 10:16:07 -0500

    .

--- ADAS/src/ada1_8/Vaca.java
+++ ADAS/src/ada1_8/Vaca.java
@@ -4,9 +4,10 @@ package ada1_8;
  *
  * @author HikingCarrot7
  */
-public class Vaca
+public class Vaca extends Mascota
 {
 
+    @Override
     public String habla()
     {
         return "Las vacas hacen muuu";
