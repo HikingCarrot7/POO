883be07762bb5fa1fd83f0270ba170771e275cb8
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-10-03 10:13:54 -0500

    .

---
+++ ADAS/src/ejer1_15/Banco.java
@@ -0,0 +1,22 @@
+package ejer1_15;
+
+/**
+ *
+ * @author HikingCarrot7
+ */
+public class Banco
+{
+
+    private int numberOfCustomers;
+
+    public void addCustomer(Cliente cliente)
+    {
+
+    }
+
+    public int getNumOfCustomers()
+    {
+        return 0;
+    }
+
+}
